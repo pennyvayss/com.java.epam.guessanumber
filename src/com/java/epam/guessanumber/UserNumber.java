c0ac0dafefc247d5a88e6131991fdd56f1602c69
pennyvayss <97964042+pennyvayss@users.noreply.github.com>
2022-01-18 16:58:18 +0200

    Update UserNumber.java

--- src/com/java/epam/guessanumber/UserNumber.java
+++ src/com/java/epam/guessanumber/UserNumber.java
@@ -10,10 +10,7 @@ public class UserNumber {
     public static final String YOU_GUESSED = "Congratulations!"
             + " You guessed the number.";
     public static final String GUESS_THE_NUMBER = "Guess the number: ";
-    public static final String OPENING_TITLE = "A number is chosen\"\n" +
-            "            + \" between 1 to 100.\"\n" +
-            "            + \"Guess the number\"\n" +
-            "            + \" within 5 trials.\")";
+    public static final String OPENING_TITLE = "A number is chosen between 1 to 100.Guess the number  within 10 trials." 
     public static final String THE_NUMBER_IS_GREATER = " \"The number is greater than";
     public static final String THE_NUMBER_IS_LESS = "The number is less than ";
     public static final String YOU_HAVE_EXHAUSTED = "You have exhausted K trials.";
